53530ae8a765aab339a3556eda08d9a744661709
AaronN808 <147462440+AaronN808@users.noreply.github.com>
2023-12-05 19:41:39 -0700

    docs: fixed action button javascript code

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 var currentYearElement = document.getElementById('currentYear');
 currentYearElement.innerHTML = new Date().getFullYear();
+
+function showAlert() {
+    alert('When Harry Met Sally is my current favorite movie!');
+}
+
+var alertButton = document.getElementById('btn-alert');
+
+alertButton.addEventListener('click', showAlert);
