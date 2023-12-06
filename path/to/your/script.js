8b295521e27855ea47f84a02320fd3dbaef12006
AaronN808 <147462440+AaronN808@users.noreply.github.com>
2023-12-05 19:52:34 -0700

    docs: updated script,js added hover button

--- path/to/your/script.js
+++ path/to/your/script.js
@@ -8,3 +8,20 @@ function showAlert() {
 var alertButton = document.getElementById('btn-alert');
 
 alertButton.addEventListener('click', showAlert);
+
+function changeButtonLabel() {
+    alertButton.innerText = 'You are currently hovering over the alert button!'
+}
+
+function revertButtonLabel() {
+    alertButton.innerText = 'Click me for an alert';
+}
+
+alertButton.addEventListener('mouseover', changeButtonLabel);
+alertButton.addEventListener('mouseleave', revertButtonLabel);
+
+function showAlert() {
+    alert('When Harry Met Sally is my current favorite movie!');
+}
+
+alertButton.addEventListener('click', showAlert);
